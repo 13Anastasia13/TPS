5b389af5424193921a20399b6e9cc412f76677ab
Anastasia <churaewa.anastasiya@gmail.com>
2024-03-05 20:57:38 +0500

    UI & Game Over Sceen

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,14 +9,30 @@ public class EnemyAI : MonoBehaviour
     public PlayerController player;
     private bool _isPlayerNoticed;
     public float viewAngle;
+    public float damage = 30;
+    private PlayerHealth _playerHealth;
+
     void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
+        InitComponentLinks();
         PickNewPetrolPoint();
     }
 
-    // Update is called once per frame
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
     void Update()
+    {
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        PatrolUpdate();
+        AttackUpdate();
+    }
+
+    private void NoticePlayerUpdate()
     {
         var direction = player.transform.position - transform.position;
         _isPlayerNoticed = false;
@@ -31,15 +47,13 @@ public class EnemyAI : MonoBehaviour
                 }
             }
         }
+    }
 
-        if (_isPlayerNoticed)
-        {
-            _navMeshAgent.destination = player.transform.position;
-        }
-
+    private void PatrolUpdate()
+    {
         if (!_isPlayerNoticed)
         {
-            if (_navMeshAgent.remainingDistance == 0)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
                 PickNewPetrolPoint();
             }
@@ -49,5 +63,24 @@ public class EnemyAI : MonoBehaviour
     private void PickNewPetrolPoint()
     {
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if(_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _playerHealth.DealDamage(damage * Time.deltaTime);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 30;
+    public RectTransform valueRectTransform;
+    private float _maxHealth;
+
+    public GameObject gameplayUI;
+    public GameObject gameOverScreen;
+
+    private void Start()
+    {
+        _maxHealth = health;
+        DrawHealthBar();
+    }
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            PlayerIsDead();
+        }
+        DrawHealthBar();
+    }
+
+    void PlayerIsDead()
+    {
+        gameplayUI.SetActive(false);
+        gameOverScreen.SetActive(true);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<FireballCaster>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
+    }
+
+    void DrawHealthBar()
+    {
+        valueRectTransform.anchorMax = new Vector2( health / _maxHealth , 1);
+    }
+}
